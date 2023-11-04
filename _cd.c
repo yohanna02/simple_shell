742f90a7c90531c14a9b6f2d4a0f6e564d308406
Binadam442 <sanusiadam0000@gmail.com>
2023-11-04 07:50:06 -0700

    _cd.c

---
+++ _cd.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _cd - recreate cd function
+ *
+ * @path: path
+ * Return: env change
+ */
+
+int _cd(const char *path)
+{
+	char *old_pwd = NULL;
+	char *new_pwd = NULL;
+
+	old_pwd = getcwd(NULL, 0);
+
+	if (old_pwd == NULL)
+	{
+		perror("getcwd");
+		return (1);
+	}
+
+	if (chdir(path) != 0)
+	{
+		perror("chdir");
+		free(old_pwd);
+		return (1);
+	}
+
+	new_pwd = getcwd(NULL, 0);
+
+	if (new_pwd == NULL)
+	{
+		perror("getcwd");
+		free(old_pwd);
+		return (1);
+	}
+
+	setenv("PWD", new_pwd, 1);
+	setenv("OLDPWD", old_pwd, 1);
+
+	free(old_pwd);
+	free(new_pwd);
+
+	return (0);
+}
+
